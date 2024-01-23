2173b2c831cc4d7027a29c7e8b715cd1df7e01f9
shuweic <shuweic227@gmail.com>
2024-01-23 17:01:50 -0500

    feat: 1. add abstract error class which detect error when we try to handle or add custom errorException. 2. add express async handler to detect and avoid async API bad behavior

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
 import {json} from 'body-parser';
 import {currentUserRouter} from './routes/current-user';
 import {signupRouter} from './routes/signup';
 import {signinRouter} from './routes/signin';
 import {signoutRouter} from './routes/signout';
 import {errorHandler} from './middlewares/error-handler';
+import {NotFoundError} from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -13,6 +15,11 @@ app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
 app.use(errorHandler);
 app.listen(3000, () => {
     console.log('Listening on port 3000!!!!');
