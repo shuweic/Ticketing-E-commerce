402fb0b5c95b2db4393463c61c3af7f01a7b92b1
shuweic <shuweic227@gmail.com>
2024-02-08 16:32:52 -0500

    feat: generate user JWT and cookie

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from "express";
 import {body, validationResult} from "express-validator";
 import {User} from "../models/user";
+import jwt from "jsonwebtoken";
 import {RequestValidationError} from "../errors/request-validation-error";
 import {BadRequestError} from "../errors/bad-request-error";
 
@@ -37,6 +38,17 @@ router.post(
         // Create the user
         const user = User.build({email, password});
         await user.save();
+
+        // Generate JWT
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, 'asdf');
+
+        // Store it on session object
+        req.session = {
+            jwt: userJwt
+        };
 
         res.status(201).send(user);
 
